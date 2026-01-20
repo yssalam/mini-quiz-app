375b01740c8b65df0086cf61450dda0e8b4c544e
yssalam <115963938+yssalam@users.noreply.github.com>
2026-01-20 12:14:11 +0700

    Fix import path for App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./app.css"
+import "./App.css"
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 
